8e488e478a759da69145c05f4751cb71d920c4bb
lost_fanatic <bighna2005@gmail.com>
2026-03-01 11:16:17 +0530

    Update blogRoutes.js

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -2,12 +2,21 @@ const express = require('express');
 const router = express.Router();
 const blogController = require('../controllers/blogController');
 const authMiddleware = require('../middleware/authMiddleware');
+const rateLimiter = require('../middleware/rateLimiter');
+const validate = require('../middleware/validate');
+const { postSchema, commentSchema } = require('../validators/blogValidators');
+const validateId = require('../middleware/validateId');
+const blogOwnerMiddleware = require('../middleware/blogOwnerMiddleware');
+
+// Validate route parameter
+router.param('id', validateId);
 
 router.get('/', blogController.getAllPosts);
 router.get('/:id', blogController.getPostById);
-router.post('/', authMiddleware, blogController.createPost);
-router.post('/:id/comments', authMiddleware, blogController.addComment);
-router.post('/:id/like', authMiddleware, blogController.likePost);
-router.delete('/:id', authMiddleware, blogController.deletePost);
+
+router.post('/', authMiddleware, validate(postSchema), blogController.createPost);
+router.post('/:id/comments', authMiddleware, validate(commentSchema), blogController.addComment);
+router.post('/:id/like', authMiddleware, rateLimiter, blogController.likePost);
+router.delete('/:id', authMiddleware, blogOwnerMiddleware, blogController.deletePost);
 
 module.exports = router;
